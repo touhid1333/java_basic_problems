6b368bd0026ee43f432d118fe4c8ba82c0b2fce5
towfiqulislam <hridoy.islam7916@gmail.com>
2023-05-04 01:31:50 +0600

    java loop 2 problem solved

--- src/javaBasic/JavaLoopsTwo.java
+++ src/javaBasic/JavaLoopsTwo.java
@@ -15,21 +15,32 @@ public class JavaLoopsTwo {
         int a = 0, b = 0, n = 0;
         Scanner scanner = new Scanner(System.in);
         int q = scanner.nextInt();
-        int[] values = new int[q];
+        int[][] values = new int[q][3];
 
         for (int z = 0; z < q; z++){
             a = scanner.nextInt();
             b = scanner.nextInt();
             n = scanner.nextInt();
+            values[z][0]=a;
+            values[z][1]=b;
+            values[z][2]=n;
         }
 
+        for (int k=0; k<values.length; k++){
+            calculate(values[k][0], values[k][1], values[k][2]);
+            System.out.println();
+        }
+
+    }
+
+    private static void calculate(int a, int b, int n) {
         for (int i = 0; i < n; i++) { // max ==> i = n - 1
             int sum = 0;
             for (int j = 0; j <= i; j++) {
                 sum = sum + (int) Math.pow(2, j) * b;
             }
             int total = a + sum;
-            System.out.println(total);
+            System.out.print(total+ "   ");
         }
     }
 }
