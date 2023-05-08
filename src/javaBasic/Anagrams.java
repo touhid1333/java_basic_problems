2102b0882ab57a805932b649e1ca9ecd52a7bd07
Md. Touhidur Rahman <touhidur@primetechbd.com>
2023-05-08 19:50:16 +0600

    Pallindrome problem solve

---
+++ src/javaBasic/Anagrams.java
@@ -0,0 +1,21 @@
+package javaBasic;
+
+import java.util.Scanner;
+
+public class Anagrams {
+
+    public static void main(String[] args) {
+        checkAnagrams();
+    }
+
+    public static void checkAnagrams() {
+        Scanner scanner = new Scanner(System.in);
+        String word1 = scanner.nextLine();
+        String word2 = scanner.nextLine();
+        scanner.close();
+
+
+    }
+
+
+}
